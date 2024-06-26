c2fb6222dfb079ecf0abb58a50c3acd98f910ba9
PRO-FesseR <a.hadi1999.ah@gmail.com>
2024-06-27 02:36:59 +0500

    Implement functions to Handle Input

---
+++ src/Compnents/ResultsComponent.jsx
@@ -0,0 +1,17 @@
+export default function ResultsComponent(){
+    return(
+        <table>
+            <tbody>
+            <tr>
+                <thead>Year</thead>
+                <thead>Investment Value</thead>
+                <thead>Interest (Year)</thead>
+                <thead>Total Interest</thead>
+                <thead>Invested Capital</thead>
+            </tr>
+            </tbody>
+
+
+        </table>
+    )
+}
